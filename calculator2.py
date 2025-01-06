89467885256b0b9ead5b0808d44ae0b933d58393
Hemi1234567 <hemiunderscore@gmail.com>
2025-01-06 14:20:23 +1100

    Add files via upload

---
+++ calculator2.py
@@ -0,0 +1,34 @@
+
+
+operator = input("choose an operator (/, -, *, + ) ")
+
+
+num1 = float(input("Choose your first number"))
+num2 = float(input("Choose your second number"))
+if operator == "+":
+    result = num1 + num2
+    print(result)
+
+
+
+if operator == "/":
+    result = num1 / num2
+    
+    print(result)
+    
+    
+    
+if operator == "-":
+    result = num1 - num2
+    
+    print(result)
+    
+    
+    
+    
+if operator == "*":
+    result = num1 * num2
+    
+    print(result)
+    
+    
